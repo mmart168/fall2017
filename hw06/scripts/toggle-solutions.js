c9b0b38b7b408d48543561f8a17423aaf585be2c
Sarah <vanwars@gmail.com>
2017-10-27 21:56:09 -0700

    updating solutions

--- hw06/scripts/toggle-solutions.js
+++ hw06/scripts/toggle-solutions.js
@@ -6,6 +6,14 @@
 // Now, write a conditional statement. It should check to see if the classList
 // on the element contains the 'active' class
 // If it does, remove the 'active' class. If it doesn't add the 'active' class.
+const toggleLike = () => {
+    const elem1 = document.getElementById('like1')
+    if (elem1.classList.contains('active')) {
+        elem1.classList.remove('active')
+    } else {
+        elem1.classList.add('active')
+    }
+}
 
 
 // ------------------------------------
@@ -15,3 +23,7 @@
 // It should find a DOM element with the id 'like'
 // Without writing a conditional statement,
 // use the classList to toggle the 'active class'
+const toggle = () => {
+    const elem2 = document.getElementById('like2')
+    elem2.classList.toggle('active')
+}
